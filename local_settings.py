770879f118732fe3cf6afc35a6f3aa2ee1c51f36
Regina <laulessr@gmail.com>
2015-01-11 18:07:30 -0600

    dumped kim

--- local_settings.py
+++ local_settings.py
@@ -8,7 +8,7 @@ MY_CONSUMER_SECRET = '5NoKwp3gYf0RQKEAFIUOVf8SO2tYnSIu0oxU6OIZL5aDCmcnMH'
 MY_ACCESS_TOKEN_KEY = '2975326776-G5B3QRiFkuehi0JRSfG5rCaton3Wed2rjgq3ByV'
 MY_ACCESS_TOKEN_SECRET = 'I6bwATiA51NJD4PYAYEOfB921WbVy5LvinTKu79SLE8gm'
 
-SOURCE_ACCOUNTS = ["KimKardashian", "neiltyson"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+SOURCE_ACCOUNTS = ["neiltyson"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 4 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = False #Set this to False to start Tweeting live
